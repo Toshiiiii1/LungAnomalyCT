c1a36e172835d35a01e2545a88f1f1ff038693c2
Toshi <huynhduykhoi619@gmail.com>
2024-08-24 16:30:13 +0700

    store Lung image and ROI coor

--- LUNA_mask_extraction.py
+++ LUNA_mask_extraction.py
@@ -4,9 +4,48 @@ import csv
 from glob import glob
 import pandas as pd
 import os
+from tqdm import tqdm
+
+# LUNA_mask_extraction.py
 
 file_list = os.listdir("./subset0")
 file_list = list(map(lambda file : "./subset0/" + file, file_list))
+
+def make_mask(center, diam, z, width, height, spacing, origin):
+    '''
+        Center : centers of circles px -- list of coordinates x,y,z
+        diam : diameters of circles px -- diameter
+        widthXheight : pixel dim of image
+        spacing = mm/px conversion rate np array x,y,z
+        origin = x,y,z mm np.array
+        z = z position of slice in world coordinates mm
+    '''
+    mask = np.zeros([height, width]) # 0's everywhere except nodule swapping x,y to match img
+    #convert to nodule space from world coordinates
+
+    # Defining the voxel range in which the nodule falls
+    v_center = (center - origin)/spacing
+    v_diam = int(diam/spacing[0] + 1)
+    v_xmin = np.max([0, int(v_center[0]-v_diam) - 1])
+    v_xmax = np.min([width-1, int(v_center[0]+v_diam) + 1])
+    v_ymin = np.max([0, int(v_center[1]-v_diam) - 1]) 
+    v_ymax = np.min([height-1, int(v_center[1]+v_diam) + 1])
+
+    v_xrange = range(v_xmin, v_xmax+1)
+    v_yrange = range(v_ymin, v_ymax+1)
+
+    # Convert back to world coordinates for distance calculation
+    x_data = [x*spacing[0]+origin[0] for x in range(width)]
+    y_data = [x*spacing[1]+origin[1] for x in range(height)]
+
+    # Fill in 1 within sphere around nodule
+    for v_x in v_xrange:
+        for v_y in v_yrange:
+            p_x = spacing[0]*v_x + origin[0]
+            p_y = spacing[1]*v_y + origin[1]
+            if np.linalg.norm(center - np.array([p_x, p_y, z])) <= diam:
+                mask[int((p_y-origin[1]) / spacing[1]), int((p_x-origin[0]) / spacing[0])] = 1.0
+    return(mask, [v_xmin, v_xmax, v_ymin, v_ymax])
 
 def get_filename(case):
     global file_list
@@ -29,24 +68,36 @@ if __name__ == "__main__":
     df_node["file"] = df_node["seriesuid"].apply(get_filename)
     df_node = df_node.dropna()
     
-    fcount = 0
-    for img_file in file_list:
+    df_roi = dict()
+    
+    for fcount, img_file in enumerate(tqdm(file_list)):
         # print("Getting mask for image file %s" % img_file.replace("./subset0",""))
         mini_df = df_node[df_node["file"]==img_file] #get all nodules associate with file
         if len(mini_df) > 0:       # some files may not have a nodule--skipping those
             itk_img = sitk.ReadImage(img_file) # read .mhd file
             img_array = sitk.GetArrayFromImage(itk_img) # indexes are z,y,x (notice the ordering)
-            numz, height, width = img_array.shape
-            center = np.array([node_x, node_y, node_z])   # nodule center
+            num_z, height, width = img_array.shape
             origin = np.array(itk_img.GetOrigin())      # x,y,z  Origin in world coordinates (mm)
             spacing = np.array(itk_img.GetSpacing())    # spacing of voxels in world coor. (mm)
-            v_center = np.rint((center-origin)/spacing)  # nodule center in voxel space (still x,y,z ordering
             
-            i = 0
-            for i_z in range(int(v_center[2])-1, int(v_center[2])+2):
-                mask = make_mask(center, diam, i_z*spacing[2]+origin[2], width, height, spacing, origin)
+            for node_idx, cur_row in mini_df.iterrows():       
+                node_x = cur_row["coordX"]
+                node_y = cur_row["coordY"]
+                node_z = cur_row["coordZ"]
+                diam = cur_row["diameter_mm"]
+                
+                imgs = np.ndarray([5, height, width], dtype=np.float32)
+                masks = np.ndarray([5, height, width], dtype=np.uint8)
+                center = np.array([node_x, node_y, node_z])   # nodule center
+                v_center = np.rint((center-origin)/spacing)  # nodule center in voxel space (still x,y,z ordering)
+            
+            for i, i_z in enumerate(np.arange(int(v_center[2])-2, int(v_center[2])+3).clip(0, num_z-1)): # clip prevents going out of bounds in Z
+                mask, roi = make_mask(center, diam, i_z*spacing[2]+origin[2], width, height, spacing, origin)
+                if i == 1:
+                    temp = "roi_%04d_%04d" % (fcount, node_idx)
+                    df_roi.update({temp: roi})
                 masks[i] = mask
-                imgs[i] = matrix2int16(img_array[i_z])
-                i+=1
-            np.save(output_path+"images_%d.npy" % (fcount) ,imgs)
-            np.save(output_path+"masks_%d.npy" % (fcount) ,masks)
+                imgs[i] = img_array[i_z]
+            np.save(os.path.join("./images", "images_%04d_%04d.npy" % (fcount, node_idx)), imgs)
+            np.save(os.path.join("./masks", "masks_%04d_%04d.npy" % (fcount, node_idx)), masks)
+        
