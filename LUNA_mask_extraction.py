8437e6095c8d27789d5f8dace88a466eb656be60
Toshi <huynhduykhoi619@gmail.com>
2024-09-24 20:41:21 +0700

    refactor dataset

--- LUNA_mask_extraction.py
+++ LUNA_mask_extraction.py
@@ -7,8 +7,10 @@ import os
 from tqdm import tqdm
 import cv2
 
-file_list = os.listdir("./subset9")
-file_list = list(map(lambda file : "./subset9/" + file, file_list))
+# LUNA_mask_extraction.py
+subset = "subset0"
+file_list = os.listdir(f"./LUNA16/{subset}")
+file_list = list(map(lambda file : f"./LUNA16/{subset}/" + file, file_list))
 
 def make_mask(center, diam, z, width, height, spacing, origin):
     '''
@@ -33,10 +35,6 @@ def make_mask(center, diam, z, width, height, spacing, origin):
     v_xrange = range(v_xmin, v_xmax+1)
     v_yrange = range(v_ymin, v_ymax+1)
 
-    # Convert back to world coordinates for distance calculation
-    x_data = [x*spacing[0]+origin[0] for x in range(width)]
-    y_data = [x*spacing[1]+origin[1] for x in range(height)]
-
     # Fill in 1 within sphere around nodule
     for v_x in v_xrange:
         for v_y in v_yrange:
@@ -59,26 +57,16 @@ def normalize(image):
     image[image>1] = 1.
     image[image<0] = 0.
     return image
-        
-def matrix2int16(matrix):
-    ''' 
-        matrix must be a numpy array NXN
-        Returns uint16 version
-    '''
-    m_min= np.min(matrix)
-    m_max= np.max(matrix)
-    matrix = matrix-m_min
-    return(np.array(np.rint( (matrix-m_min)/float(m_max-m_min) * 65535.0), dtype=np.uint16))
 
 if __name__ == "__main__":
     df_node = pd.read_csv("./LUNA16/annotations.csv")
     df_node["file"] = df_node["seriesuid"].apply(get_filename)
     df_node = df_node.dropna()
-    df_roi = pd.read_csv("./ROI_coor.csv")
+    df_roi = pd.read_csv("./ROI_coor3.csv")
     df_roi_cur = dict()
     
+    print(f"TRÍCH XUẤT ẢNH VÀ TỌA ĐỘ TỪ FILE .raw VÀ FILE .mhd TỪ {subset}")
     for fcount, img_file in enumerate(tqdm(file_list)):
-        # print("Getting mask for image file %s" % img_file.replace("./subset0",""))
         mini_df = df_node[df_node["file"]==img_file] #get all nodules associate with file
         if len(mini_df) > 0:       # some files may not have a nodule--skipping those
             itk_img = sitk.ReadImage(img_file) # read .mhd file
@@ -94,38 +82,39 @@ if __name__ == "__main__":
                 diam = cur_row["diameter_mm"]
                 
                 imgs = np.ndarray([5, height, width], dtype=np.float32)
-                # masks = np.ndarray([5, height, width], dtype=np.uint8)
+                masks = np.ndarray([5, height, width], dtype=np.uint8)
                 center = np.array([node_x, node_y, node_z])   # nodule center
                 v_center = np.rint((center-origin)/spacing)  # nodule center in voxel space (still x,y,z ordering)
             
                 for i, i_z in enumerate(np.arange(int(v_center[2])-2, int(v_center[2])+3).clip(0, num_z-1)): # clip prevents going out of bounds in Z
-                    # mask, roi = make_mask(center, diam, i_z*spacing[2]+origin[2], width, height, spacing, origin)
-                    _, roi = make_mask(center, diam, i_z*spacing[2]+origin[2], width, height, spacing, origin)
+                    mask, roi = make_mask(center, diam, i_z*spacing[2]+origin[2], width, height, spacing, origin)
                     if i == 2:
-                        # roi_key = "roi_%04d_%04d" % (fcount, node_idx)
                         roi_key = f"{img_file.split('/')[-1].rsplit('.', 1)[0]}_{fcount}_{node_idx}"
                         df_roi_cur.update({roi_key: roi})
-                    # masks[i] = mask
                     imgs[i] = img_array[i_z]
+                    masks[i] = mask
                 img_name = f"{img_file.split('/')[-1].rsplit('.', 1)[0]}_{fcount}_{node_idx}.npy"
                 mask_name = f"{img_file.split('/')[-1].rsplit('.', 1)[0]}_{fcount}_{node_idx}.npy"
-                # np.save(os.path.join("./images", "images_%04d_%04d.npy" % (fcount, node_idx)), imgs)
-                # np.save(os.path.join("./masks", "masks_%04d_%04d.npy" % (fcount, node_idx)), masks)
                 
-                np.save(os.path.join("./LUNA16/images", img_name), imgs)
-                # np.save(os.path.join("./masks", mask_name), masks)
-            
+                if os.path.isdir(f"./LUNA16/images/{subset}") is False and os.path.isdir(f"./LUNA16/masks/{subset}") is False:
+                    os.mkdir(f"./LUNA16/images/{subset}")
+                    os.mkdir(f"./LUNA16/masks/{subset}")
+                    np.save(os.path.join(f"./LUNA16/images/{subset}", img_name), imgs)
+                    np.save(os.path.join(f"./LUNA16/masks/{subset}", mask_name), masks)
+                else:
+                    np.save(os.path.join(f"./LUNA16/images/{subset}", img_name), imgs)
+                    np.save(os.path.join(f"./LUNA16/masks/{subset}", mask_name), masks)
+         
     df_temp = pd.DataFrame.from_dict(df_roi_cur, columns=["x_min", "x_max", "y_min", "y_max"], orient='index')
     df_temp["ID"] = list(df_roi_cur.keys())
     df_temp = df_temp.reset_index(drop=True)
     df_roi = pd.concat([df_roi, df_temp])
-    df_roi.to_csv("ROI_coor.csv", index=False)
+    df_roi.to_csv("ROI_coor3.csv", index=False)
     
-    img_files = os.listdir("./LUNA16/images")
+    img_files = os.listdir(f"./LUNA16/images/{subset}")
     for img_file in tqdm(img_files):
-        imgs = np.load(f"./LUNA16/images/{img_file}")
-        temp = normalize(imgs[2])
-        img_grey = temp * 255
+        imgs = np.load(f"./LUNA16/images/{subset}/{img_file}")
+        img = normalize(imgs[2])
+        img_grey = img * 255
         img_rgb = np.stack((img_grey,)*3, -1)
-        cv2.imwrite(f"./preprocessed_img/{img_file.rsplit('.', 1)[0]}.png", img_rgb)
-        
+        cv2.imwrite(f"./preprocessed_img2/{img_file.rsplit('.', 1)[0]}.png", img_rgb)
--- yolo_input_prepare.py
+++ yolo_input_prepare.py
@@ -10,7 +10,7 @@ def yolo_format_calculate(x_min, x_max, y_min, y_max, width, height):
     return x_center/width, y_center/height, box_width/width, box_height/height
 
 if __name__ == "__main__":
-    df_roi = pd.read_csv("./ROI_coor2.csv")
+    df_roi = pd.read_csv("./ROI_coor3.csv")
     for idx, row in tqdm(df_roi.iterrows()):
         x_min = row["x_min"]
         x_max = row["x_max"]
@@ -20,6 +20,6 @@ if __name__ == "__main__":
         
         x_center, y_center, box_w, box_h = yolo_format_calculate(x_min, x_max, y_min, y_max, 512, 512)
         
-        with open(f"./labels/{roi_id}.txt", "w") as f:
+        with open(f"./labels2/{roi_id}.txt", "w") as f:
             f.write(f"0 {x_center} {y_center} {box_w} {box_h}")
         
