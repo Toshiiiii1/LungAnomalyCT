42801d126855ba22cf99ada6aeee8614bde2490c
Toshi <huynhduykhoi619@gmail.com>
2025-04-18 15:03:47 +0700

    reconstruct data folder

---
+++ data/preprocessed/FRCNN format/lung_ct_version_n_512.v2i.voc/README.dataset.txt
@@ -0,0 +1,6 @@
+# lung_ct_version_n > 2024-11-03 12:52pm
+https://universe.roboflow.com/water-meter-k29v4/lung_ct_version_n
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
---
+++ data/preprocessed/yolo_input_prepare.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from tqdm import tqdm
+
+def yolo_format_calculate(x_min, x_max, y_min, y_max, width, height):
+    box_width = x_max - x_min
+    box_height = y_max - y_min
+    x_center = x_min + (box_width/2)
+    y_center = y_min + (box_height/2)
+
+    return x_center/width, y_center/height, box_width/width, box_height/height
+
+if __name__ == "__main__":
+    df_roi = pd.read_csv("./ROI_coor4.csv")
+    for idx, row in tqdm(df_roi.iterrows()):
+        x_min = row["x_min"]
+        x_max = row["x_max"]
+        y_min = row["y_min"]
+        y_max = row["y_max"]
+        roi_id = row["ID"]
+        
+        x_center, y_center, box_w, box_h = yolo_format_calculate(x_min, x_max, y_min, y_max, 512, 512)
+        
+        with open(f"./labels3/{roi_id}.txt", "w") as f:
+            f.write(f"0 {x_center} {y_center} {box_w} {box_h}")
+        
