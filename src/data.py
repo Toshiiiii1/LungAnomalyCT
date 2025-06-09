1e97771f83593eba568e709d6b04908262ec1bca
Toshi <huynhduykhoi619@gmail.com>
2025-06-09 15:50:19 +0700

    split data class to another file

---
+++ src/data.py
@@ -0,0 +1,123 @@
+import os
+import torch
+import cv2
+import numpy as np
+import albumentations as A
+
+from torch.utils.data import DataLoader, Dataset
+from xml.etree import ElementTree as et
+
+class LungImagesDataset(Dataset):
+
+    def __init__(self, files_dir, width, height, is_train):
+        self.is_train = is_train
+        self.files_dir = files_dir
+        self.height = height
+        self.width = width
+        
+        # sorting the images for consistency
+        # To get images, the extension of the filename is checked to be jpg
+        self.imgs = [image for image in sorted(os.listdir(files_dir))
+                        if image[-4:]=='.jpg']
+        
+        
+        # classes: 0 index is reserved for background
+        self.classes = ["background", 'nodule']
+        
+    def transform(self):
+        if self.is_train:
+            return A.Compose([
+                                A.HorizontalFlip(0.5),
+                        # ToTensorV2 converts image to pytorch tensor without div by 255
+                                A.ToTensorV2(p=1.0) 
+                            ])
+        else:
+            return A.Compose([
+                                A.ToTensorV2(p=1.0)
+                            ])
+
+    def __getitem__(self, idx):
+
+        img_name = self.imgs[idx]
+        image_path = os.path.join(self.files_dir, img_name)
+
+        # reading the images and converting them to correct size and color    
+        img = cv2.imread(image_path)
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB).astype(np.float32)
+        img_res = cv2.resize(img_rgb, (self.width, self.height), cv2.INTER_AREA)
+        # diving by 255
+        img_res /= 255.0
+        
+        # annotation file
+        annot_filename = img_name[:-4] + '.xml'
+        annot_file_path = os.path.join(self.files_dir, annot_filename)
+        
+        boxes = []
+        labels = []
+        tree = et.parse(annot_file_path)
+        root = tree.getroot()
+        
+        # cv2 image gives size as height x width
+        wt = img.shape[1]
+        ht = img.shape[0]
+        
+        # box coordinates for xml files are extracted and corrected for image size given
+        for member in root.findall('object'):
+            labels.append(self.classes.index(member.find('name').text))
+            
+            # bounding box
+            xmin = int(member.find('bndbox').find('xmin').text)
+            xmax = int(member.find('bndbox').find('xmax').text)
+            
+            ymin = int(member.find('bndbox').find('ymin').text)
+            ymax = int(member.find('bndbox').find('ymax').text)
+            
+            
+            xmin_corr = (xmin/wt)*self.width
+            xmax_corr = (xmax/wt)*self.width
+            ymin_corr = (ymin/ht)*self.height
+            ymax_corr = (ymax/ht)*self.height
+            
+            boxes.append([xmin_corr, ymin_corr, xmax_corr, ymax_corr])
+        
+        # convert boxes into a torch.Tensor
+        boxes = torch.as_tensor(boxes, dtype=torch.float32)
+        
+        # getting the areas of the boxes
+        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+
+        # suppose all instances are not crowd
+        iscrowd = torch.zeros((boxes.shape[0],), dtype=torch.int64)
+        
+        # labels = torch.as_tensor(labels, dtype=torch.int64)
+        labels = torch.ones((len(labels),), dtype=torch.int64)
+
+
+        target = {}
+        target["boxes"] = boxes
+        target["labels"] = labels
+        target["area"] = area
+        target["iscrowd"] = iscrowd
+        # image_id
+        # image_id = torch.tensor([idx])
+        target["image_id"] = idx
+        
+        sample = self.transforms(image = img_res)
+        img_res = sample['image']
+
+
+        # if self.transforms:
+            
+        #     # sample = self.transforms(image = img_res,
+        #     #                          bboxes = target['boxes'],
+        #     #                          labels = labels)
+
+        #     sample = self.transforms(image = img_res)
+            
+        #     img_res = sample['image']
+        #     # target['boxes'] = torch.Tensor(sample['bboxes'])
+        
+        return img_res, target
+
+    def __len__(self):
+        return len(self.imgs)
